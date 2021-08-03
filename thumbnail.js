8be7ea6c63158529372c2c7d6333e6da435ae85b
hanchuangkudu666 <71386754+hanchuangkudu666@users.noreply.github.com>
2021-08-03 17:02:51 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //alert("Hello JavaScript");
+    
+    //使图片变大
+    let targetImage = document.querySelector("#smart-image");
 });
